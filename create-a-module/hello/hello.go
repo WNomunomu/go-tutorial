f740f2d7cf86d1c9f8ec7ce4f927f1d57528f54a
WNomunomu <kohsei.nmr@gmail.com>
2025-10-21 16:17:20 +0900

    Change to log errors

--- create-a-module/hello/hello.go
+++ create-a-module/hello/hello.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("r4mn0")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
 
